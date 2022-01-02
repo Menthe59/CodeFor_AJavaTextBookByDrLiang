cf3b9682da9969acff495d20b7ef6f47a95a91ff
Menthe59 <Menthe59@outlook.com>
2022-01-02 22:54:07 +0800

    chapter3

--- chapter3/src/pers/menthe59/chapter3/MainOfChapter3.java
+++ chapter3/src/pers/menthe59/chapter3/MainOfChapter3.java
@@ -30,7 +30,7 @@ public class MainOfChapter3 {
 */
 
         //quiz3.2
-/*        int num1 = (int)(Math.random() * 10);
+/*      int num1 = (int)(Math.random() * 10);
         int num2 = (int)(Math.random() * 10);
         int num3 = (int)(Math.random() * 10);
 
@@ -40,6 +40,62 @@ public class MainOfChapter3 {
         System.out.println(num1 + " + " + num2 + " + " + num3 + " = "
                 + answer + " is " + (num1 + num2  + num3 == answer));*/
 
+        //quiz3.3
+/*      System.out.println("Enter a, b, c, d, e, f:");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+        double d = input.nextDouble();
+        double e = input.nextDouble();
+        double f = input.nextDouble();
+
+        if(a*d - b*c == 0){
+            System.out.println("The equation has no solution.");
+        }else{
+            System.out.println("x is " + (e*d - b*f)/(a*d - b*c)
+                    + " and y is " + (a*f -e*c)/(a*d - b*c));
+        }*/
+
+        //quiz3.4
+/*      int randNum = (int)(Math.random() * 12 + 1);
+        switch (randNum){
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("Noverber");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+        }*/
 
 
 
