8f99bc78dae1cfa9e1628a09460f21b3ae543170
Menthe59 <33413453+Menthe59@users.noreply.github.com>
2022-01-02 16:05:32 +0800

    chapter3

---
+++ chapter3/src/pers/menthe59/chapter3/MainOfChapter3.java
@@ -0,0 +1,47 @@
+package pers.menthe59.chapter3;
+
+import java.util.Scanner;
+
+public class MainOfChapter3 {
+    public static void main(String[] args) {
+        /*Please remove the double slash used for comments before test.
+        测试相关题目前请删去对应注释。
+        */
+
+        Scanner input = new Scanner(System.in);
+
+        //quiz3.1
+/*
+        System.out.println("Enter a,b,c :");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+
+        double judgeNum = Math.pow(b, 2) - 4*a*c;
+
+        if(judgeNum < 0){
+            System.out.println("The equation has no real roots.");
+        }else if(judgeNum == 0){
+            System.out.println("The equation has one root " + -b/a*0.5);
+        }else{
+            System.out.println("The equation has two roots "
+                    + (-b+Math.pow(judgeNum,0.5)) / 2*a + " " + (-b-Math.pow(judgeNum,0.5)) / 2*a);
+        }
+*/
+
+        //quiz3.2
+/*        int num1 = (int)(Math.random() * 10);
+        int num2 = (int)(Math.random() * 10);
+        int num3 = (int)(Math.random() * 10);
+
+        System.out.println("What is " + num1 + " + " + num2 + " + " + num3 + " ?");
+        int answer = input.nextInt();
+
+        System.out.println(num1 + " + " + num2 + " + " + num3 + " = "
+                + answer + " is " + (num1 + num2  + num3 == answer));*/
+
+
+
+
+    }
+}
