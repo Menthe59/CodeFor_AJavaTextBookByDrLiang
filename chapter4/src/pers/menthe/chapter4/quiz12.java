91c1b6cbf0b4f9f246d2aee236ea6fba74b5af2d
Menthe59 <Menthe59@outlook.com>
2022-01-20 23:13:14 +0800

    chapter4

--- chapter4/src/pers/menthe/chapter4/quiz12.java
+++ chapter4/src/pers/menthe/chapter4/quiz12.java
@@ -1,4 +1,33 @@
 package pers.menthe.chapter4;
 
+import java.util.Scanner;
+
 public class quiz12 {
+    public static void main(String[] args) {
+        String hexString;
+        char hexDigit;
+        int decDigit;
+
+        System.out.println("Enter a hex digit : ");
+        Scanner input = new Scanner(System.in);
+        hexString = input.next();
+
+        if (hexString.length() != 1)
+            System.exit(1);
+        else
+        {
+            hexDigit = hexString.charAt(0);
+            if ('0' <= hexDigit && hexDigit <= '9'){
+                decDigit = Integer.parseInt(hexString);
+                System.out.println("The binary value is " + Integer.toBinaryString(decDigit));
+            }
+            else if ('A' <= hexDigit && hexDigit <= 'F'){
+                decDigit = 10 + hexDigit - 'A';
+                System.out.println("The binary value is " + Integer.toBinaryString(decDigit));
+            }
+            else
+                System.out.println(hexString + " is an invalid input");
+        }
+        input.close();
+    }
 }
